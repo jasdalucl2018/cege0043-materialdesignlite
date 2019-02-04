a217c6fa6fedbbd8120dafdd291091a2e12b52b0
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-02-04 06:43:16 -0500

    19_2_ re rhMenu.js adding function to refernce ucl logo

--- JDapp/rhMenu.js
+++
@@ -1,3 +0,0 @@
-function menu(){
-	alert ("you clicked the menu")
-}
---
+++ JDapp/www/rhMenu.js
@@ -0,0 +1,7 @@
+function menu(){
+	alert ("you clicked the menu")
+}
+
+function addUCLLogo(){
+	document.getElementByld("ucllogo").innerHTML="<img scr='images/ucl.png>"
+}
