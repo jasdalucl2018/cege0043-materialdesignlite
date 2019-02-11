f07a5e131062427983a620accabcc49242cc5a6b
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-02-11 06:28:55 -0500

    19_2_11 11:28 correcting leafLetfuction point, circle anf poly data

--- JDapp/www/js/leafletFunctions.js
+++ JDapp/www/js/leafletFunctions.js
@@ -2,28 +2,49 @@
 var client;
 // and a variable that will hold the layer itself
 var earthquakelayer;
+
 // function to add point, line and polygone to map
 function addPointLinePoly(){
 // adding code for line 
 L.marker([51.5, -0.09]).addTo(mymap):
 
-// adding polygone
-
-var myPolygon = L.polygon 	([
-	[51.509., -0.08],
-	[51.503, - 0.06],
-	[51.51, - 0.047], {
-		color: 'red'
+//function to create point, circle and line
+function addPointLinePoly(){
+	//adding a point
+	L.marker([51.5, -0.09]).addTo(mymap);
+	// add a circle
+	L.circle([51.508, -0.11], 500, {
+		color: 'red',
 		fillColor: '#f03',
 		fillOpacity: 0.5
-	}).addTo (mymap);
-// adding line
-var latlngs = [
-[51.0, -0.10],
-[51.2, -0.05]
-];
-var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
+		}).addTo(mymap).bindPopup("I am a circle.");
+		//adding a line aka 'polyline'
+		var latlngs = [
+		[51.5, -0.09],
+		[51.51, -0.047]
+		];
+		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
 }
+
+//function to create point, circle and line
+function addPointLinePoly(){
+	//adding a point
+	L.marker([51.5, -0.09]).addTo(mymap);
+	// add a circle
+	L.circle([51.508, -0.11], 500, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+		}).addTo(mymap).bindPopup("I am a circle.");
+		//adding a line aka 'polyline'
+		var latlngs = [
+		[51.5, -0.09],
+		[51.51, -0.047]
+		];
+		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
+}
+
+
 // // 19_2_11 9:25 adding getEarthquake function 
 function getEarthquakes() {
 	client = new XMLHttpRequest();
@@ -38,15 +59,6 @@ function earthquakeResponse() {
 		loadEarthquakelayer(earthquakedata);
 	}
 }
-// define a global variable to hold the layer so that we can use it later on
-var earthquakelayer;
-
-/// .. missing code here
-
-function loadEarthquakelayer(earthquakedata) {
-	var earthquakejson = JSON.parse(earthquakedata);
-	earthquakelayer = L.geoJson(earthquakejson), addTo(mymap);
-	mymap.fitBounds(earthquakelayer.getBounds());
 
 
 
