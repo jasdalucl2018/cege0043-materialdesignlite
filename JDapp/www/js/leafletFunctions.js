bac32def27cc1caee2c9c925cdfde8c74b32dd6f
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-02-11 09:31:31 -0500

    19_2_11 changes to leafletFunctions.js

--- JDapp/www/js/leafletFunctions.js
+++ JDapp/www/js/leafletFunctions.js
@@ -1,13 +1,7 @@
-// 19_2_11 9:25 adding getEarthquake function (this is a global variable fo AJAX request)
+// adding variable that holds a XMLHttpRequest() for the earthquake data
 var client;
-// and a variable that will hold the layer itself
+// and a variable that will hold the layer itself for the earthquake data
 var earthquakelayer;
-
-// function to add point, line and polygone to map
-function addPointLinePoly(){
-// adding code for line 
-L.marker([51.5, -0.09]).addTo(mymap):
-
 //function to create point, circle and line
 function addPointLinePoly(){
 	//adding a point
@@ -25,27 +19,7 @@ function addPointLinePoly(){
 		];
 		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
 }
-
-//function to create point, circle and line
-function addPointLinePoly(){
-	//adding a point
-	L.marker([51.5, -0.09]).addTo(mymap);
-	// add a circle
-	L.circle([51.508, -0.11], 500, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-		}).addTo(mymap).bindPopup("I am a circle.");
-		//adding a line aka 'polyline'
-		var latlngs = [
-		[51.5, -0.09],
-		[51.51, -0.047]
-		];
-		var polyline = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
-}
-
-
-// // 19_2_11 9:25 adding getEarthquake function 
+// create function to get Earthquakes data using an XMLHttpRequest
 function getEarthquakes() {
 	client = new XMLHttpRequest();
 	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
@@ -59,6 +33,12 @@ function earthquakeResponse() {
 		loadEarthquakelayer(earthquakedata);
 	}
 }
-
-
+// define a global variable to hold the layer so that we can use it later on
+var earthquakelayer;
+// convert the received data - which is text - to JSON format and add it to the map
+function loadEarthquakelayer(earthquakedata) {
+	var earthquakejson = JSON.parse(earthquakedata);
+	earthquakelayer = L.geoJson(earthquakejson).addTo(mymap);
+	mymap.fitBounds(earthquakelayer.getBounds());
+}
 
