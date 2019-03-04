c0e14b10e5c129d0798dbb83b93eadeda105963e
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-04 07:45:15 -0500

    19-3-4 modifyin startup.js to add function to process html when page is loaded

--- JDapp/www/js/startup.js
+++ JDapp/www/js/startup.js
@@ -7,6 +7,13 @@ function trackAndCircle(){
 
 }
 
+
+//add function to process html qwhen page is loaded
+
+function loadW3HTML(){
+	w3.includeHTML();
+}
+
 // add Point/Line/Circle data and track location automatically - useful for setting up different startup functions!
 function startup(){
 	document.addEventListener('DOMContentLoaded',function(){
