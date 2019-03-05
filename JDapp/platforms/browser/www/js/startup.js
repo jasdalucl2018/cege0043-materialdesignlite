3bb7616f6fc89d9282690de2c57f6d8a53947f4a
jason Dalrymnple <ucfajhd@ucl.ac.uk>
2019-03-05 08:13:28 -0500

    19-4-5 ammending code to index.html file

--- JDapp/platforms/browser/www/js/startup.js
+++ JDapp/platforms/browser/www/js/startup.js
@@ -7,11 +7,19 @@ function trackAndCircle(){
 
 }
 
+
+//add function to process html qwhen page is loaded
+
+function loadW3HTML(){
+	w3.includeHTML();
+}
+
 // add Point/Line/Circle data and track location automatically - useful for setting up different startup functions!
 function startup(){
 	document.addEventListener('DOMContentLoaded',function(){
 		trackAndCircle();
 		getPort();
+		loadW3HTML
 	},false);
 }
 
---
+++ JDapp/platforms/browser/www/js/uploadData.js
@@ -0,0 +1,80 @@
+function startDataUpload() {
+    alert ("start data upload")
+    
+    // 19-3-1 @12;31 amending function to get values from form and post mit to server
+
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var module = document.getElementById("module").value;
+    var postString = "name=" + name + "&surname=" + surname + "&module=" + module;
+
+
+    // 19-3-1 adding get the checkbox values - separate them with a | so that they can be
+    // split later on if necessary
+    var checkString = "";
+    for (var i = 1;i< 5;i++){
+        if (document.getElementById("check"+i).checked === true) {
+            checkString = checkString +
+            document.getElementById("check"+i).value + "||"
+        }
+    }
+    postString = postString + "&modulelist="+checkString;
+
+    // get the radio button values
+    if (document.getElementById("morning").checked) {
+        postString=postString+"&lecturetime=morning";
+    }
+    if (document.getElementById("afternoon").checked) {
+        postString=postString+"&lecturetime=afternoon";
+    }
+
+    // get the select box values
+    var language = document.getElementById("languageselectbox").value;
+    postString = postString + "&language="+language;
+
+
+    // include Lat & Long values
+
+    var latitude = document.getElementById("latitude").value;
+    var longitude = document.getElementById("longitude").value;
+    postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
+
+
+    // 19-3-3 @12:37 adding code for pop up alert
+
+    alert (postString);
+
+    // 19-3-1 @ 13;17 confirm that function actually runs
+
+
+    processData(postString);
+
+}
+
+// 19-3-1 @13;03 adding global variable to hold Ajax request
+
+var client; 
+
+// 190-3-1 @13;04 creating function request to upload data.
+
+function processData(postString){
+    client = new XMLHttpRequest();
+
+    postString = postString + "&port id=" + httpPortNumber;
+    var url = 'http://developer.cege.ucl.ac.uk:' + httpPortNumber + "/uploadData";
+    client.open('POST',ucl,true);    
+    client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    client.onreadystatechange = dataUploaded;
+    client.send(postString);
+}
+
+// 19-3-1 @13:04 creating code to wait for response from server and process response once it is received
+
+function dataUploaded(){
+    // function listens for server to say data is readyState
+    if (client.readyState === 4){
+        // change data upload result DIV to show response
+        document.getElementById("dataUploadResult").innerHTML = client.responseText;
+    }
+}
+
